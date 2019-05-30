06478efc1dc0226c52d07987970f4e6e8282f86b
roberto <roberto.rossetti@unicaspa.it>
2019-05-30 17:34:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview ES7 wrapper for the axios-redmine package.
+ */
+
+'use strict';
+
+const Redmine = require('../lib/redmine');
+
+class RedmineAsync {
+
+    _redmine;
+
+    constructor(hostname, config) {
+        this._redmine = new Redmine(hostname, config);
+    }
+
+    issue(params) {
+        return new Promise((resolve, reject)=> {
+            this._redmine.issue(params).then((result) => {
+                resolve(result);
+            }).catch((error) => {
+                reject(error);
+            })
+        });
+    }
+}
